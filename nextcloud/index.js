9da8072be9b26f74b0ab6c2169454342ba78bb92
hoholee12 <hoholee12@gmail.com>
2022-06-17 03:43:05 -0700

    a

---
+++ nextcloud/index.js
@@ -0,0 +1,52 @@
+var INotifyWait = require('inotifywait');
+const { exec } = require('child_process');
+const execSync = require('child_process').execSync;
+
+var starttime = 0;
+//run test
+var hrTime = process.hrtime();
+starttime = hrTime[0] * 1000 + hrTime[1] / 1000000
+execSync('dd if=/dev/urandom of=/home/test/Nextcloud/testfile bs=1M count=3', {encoding:'utf-8'});
+
+var watch1 = new INotifyWait('/var/www/html/nextcloud/data/test/files', { recursive: true });
+watch1.on('ready', function (filename) {
+  console.log('inotify is watching');
+});
+watch1.on('add', function (filename) {
+  var hrTime = process.hrtime();
+  var N = hrTime[0] * 1000 + hrTime[1] / 1000000
+    console.log(filename + ' add took ' + (N - starttime) + ' miliseconds');
+  if(filename.includes("ocTransfer")){
+    console.log(filename + ' add took ' + (N - starttime) + ' miliseconds');
+    throw ''
+  }
+});
+watch1.on('change', function (filename) {
+  var hrTime = process.hrtime();
+  var N = hrTime[0] * 1000 + hrTime[1] /1000000
+    console.log(filename + ' change took ' + (N - starttime) + ' miliseconds');
+//  if(filename.includes("ocTransfer")){
+    console.log(filename + ' change took ' + (N - starttime) + ' miliseconds');
+    throw ''
+//  }
+});
+watch1.on('unlink', function (filename) {
+  var hrTime = process.hrtime();
+  var N = hrTime[0] * 1000 + hrTime[1] /1000000
+    console.log(filename + ' unlink took ' + (N - starttime) + ' miliseconds');
+  if(filename.includes("ocTransfer")){
+    console.log(filename + ' unlink took ' + (N - starttime) + ' miliseconds');
+    throw ''
+  }
+});
+watch1.on('unknown', function (filename) {
+  var hrTime = process.hrtime();
+  var N = hrTime[0] * 1000 + hrTime[1] /1000000
+    console.log(filename + ' unknown took ' + (N - starttime) + ' miliseconds');
+  if(filename.includes("ocTransfer")){
+    console.log(filename + ' unknown took ' + (N - starttime) + ' miliseconds');
+    throw ''
+  }
+});
+
+
